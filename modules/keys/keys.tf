b3c68182a890aa2948cec934af23df898e520762
gui-secops <guilherme.devsecops@gmail.com>
2025-03-03 19:13:27 -0300

    fix: keys

--- modules/keys/keys.tf
+++ modules/keys/keys.tf
@@ -3,16 +3,6 @@ resource "tls_private_key" "ec2_key" {
   rsa_bits  = 4096
 }
 
-resource "local_file" "private_key" {
-  content  = tls_private_key.ec2_key.private_key_pem
-  filename = "./.ssh/terraform_rsa"
-}
-
-resource "local_file" "public_key" {
-  content  = tls_private_key.ec2_key.public_key_openssh
-  filename = "./.ssh/terraform_rsa.pub"
-}
-
 resource "aws_key_pair" "ec2_key_pair" {
   key_name   = "${var.projeto}-${var.candidato}-key"
   public_key = tls_private_key.ec2_key.public_key_openssh
